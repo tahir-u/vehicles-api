d6daf5c7890346e57384ce4b93a7d5f85909f181
tahir-u <tahiru1994@outlook.com>
2018-09-08 20:14:36 -0400

    Add the remaining initial route implementations

--- lib/routes/vehicleRoutes.ts
+++ lib/routes/vehicleRoutes.ts
@@ -9,5 +9,42 @@ export class Routes {
                     message: "GET request successful"
                 })
             })
+
+        app.route("/vehicle")
+            // Get all vehicles
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: "Get request successful"
+                })
+            })
+
+            // Create a new vehicle
+            .post((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: "POST request successful"
+                })
+            })
+
+        app.route("/vehicle/:id")
+            // Get a single vehicle's details
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: "GET request successful"
+                })
+            })
+
+            // Update a single vehicle
+            .put((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: "PUT request successful"
+                })
+            })
+
+            // Delete a single vehicle
+            .delete((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: "DELETE request successful"
+                })
+            })
     }
 }
