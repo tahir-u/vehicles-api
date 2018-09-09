dd4f79090b2660477f3e070b1e464c2d09ef724c
tahir-u <tahiru1994@outlook.com>
2018-09-08 21:17:03 -0400

    Add mongodb connection functionality

--- lib/app.ts
+++ lib/app.ts
@@ -1,16 +1,19 @@
 
 import * as express from "express"
 import * as bodyParser from "body-parser"
+import * as mongoose from "mongoose"
 
 import { Routes } from "./routes/vehicleRoutes"
 
 class App {
     public app: express.Application
     public route: Routes = new Routes()
+    public mongoUrl: string = "mongodb://localhost/vehicleDb"
 
     constructor() {
         this.app = express()
         this.config()
+        this.mongoSetup()
         this.route.routes(this.app)
     }
 
@@ -18,6 +21,11 @@ class App {
         this.app.use(bodyParser.json())
 
         this.app.use(bodyParser.urlencoded({ extended: false }))
+    }
+
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise
+        mongoose.connect(this.mongoUrl)
     }
 }
 
