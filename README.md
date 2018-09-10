1eb33393d961234e9623c09a700ed395b809b890
tahir-u <tahiru1994@outlook.com>
2018-09-10 15:30:31 -0400

    Update README with route information
    * Update routes with /v1 prefix
    * Add Routes section to README
    * Remove base route

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ npm install -g typescript ts-node
 
 #### Overview
 This NodeJS, Express, and MongoDB driven Web API exposes a collection of Vehicle objects that can be operated on via normal RESTful operations
+
+#### Routes
+To access the routes available with this Web API, run `npm run dev` to run the Express server locally. Once the server comes up, navigate to `http://localhost:3000/v1/vehicle` to view the data that was loaded from the `vehicles.json` file in `data/`.
+
+The other routes available for this Web API are:
+* `/v1/vehicle/:id` (`GET`): View a specific vehicle, indexed by its ID
+* `/v1/vehicle/:id` (`POST`): Update a specific vehicle's information, indexed by its ID
+* `/v1/vehicle/:id` (`DELETE`): Delete a specific vehicle, indexed by its ID
--- lib/routes/vehicleRoutes.ts
+++ lib/routes/vehicleRoutes.ts
@@ -7,21 +7,15 @@ export class Routes {
     public vehicleController: VehicleController = new VehicleController()
 
     public routes(app): void {
-        app.route("/")
-            .get((res: Response) => {
-                res.status(200).send({
-                    message: "GET request successful"
-                })
-            })
 
-        app.route("/vehicle")
+        app.route("/v1/vehicle")
             // Get all vehicles
             .get(this.vehicleController.getVehicles)
 
             // Create a new vehicle
             .post(this.vehicleController.addNewVehicle)
 
-        app.route("/vehicle/:id")
+        app.route("/v1/vehicle/:id")
             // Get a single vehicle's details
             .get(this.vehicleController.getVehicleWithId)
 
